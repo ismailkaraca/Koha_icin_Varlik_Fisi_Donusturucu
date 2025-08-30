5ab6bebf0af6824f4342ba557a795393c9cee800
ismailkaraca <ismail.krc@gmail.com>
2025-08-30 23:46:07 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -213,7 +213,7 @@ const App = () => {
                             <div>
                                 <p className="font-bold">Kullanım Bilgisi</p>
                                 <p className="text-sm">
-                                    Burada oluşturulan dosyanın "Koha Kütüphane Otomasyon Sistemi"nde nasıl kullanılacağına dair bilgiye, 
+                                    Burada oluşturulan dosyanın "Koha Kütüphane Otomasyon Sistemi"nde nasıl kullanılacağına dair bilgiye,  
                                     <a 
                                         href="https://drive.google.com/file/d/1-1lRqTQSUk3dcTwZsMXQi6kXK-kDdEty/view?usp=sharing" 
                                         target="_blank" 
@@ -221,7 +221,7 @@ const App = () => {
                                         className="font-semibold underline hover:text-blue-900"
                                     >
                                         "Genel Kullanım Kılavuzu"
-                                    </a> 
+                                    </a>  
                                     içindeki "KBS Taşınır Kayıt ve Yönetim Sisteminden Veri Aktarma İşlemleri" başlığından ulaşabilirsiniz.
                                 </p>
                             </div>
@@ -363,4 +363,5 @@ const App = () => {
 };
 
 export default App;
+
 
